ba73da51c237633e1a6a3e935a3111e4bb0561f2
Eliass-g <eliass.ghauss@gmail.com>
2023-09-28 02:37:32 -0400

    final

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = '165.227.47.243';
+const PORT = '50541';
+
+const KEYS = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  x: "Say: Hi"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  KEYS
+};
